08bef57d053d4650318a53ef0f0e8118573fe5eb
Scott McCann <scottmccann@live.com>
2016-06-14 16:23:52 -0500

    Database models registered in admin.py, Added most of the 5th edition SRD items into the database.

--- wandering_lantern/tavern/views.py
+++ wandering_lantern/tavern/views.py
@@ -2,11 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Character
-
-def tavern_index(request):
-    characters = Character.objects.all()
-    templ = loader.get_template('tavern/characters.html')
-
-    context = {'films': films}
-
-    return HttpResponse(templ.render(context, request))
